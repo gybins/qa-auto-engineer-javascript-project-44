23f6fa4c63d1194bf78d8ab506eff4ae6fe55e64
Gubin Sergei <gybins@yandex.ru>
2026-06-10 19:12:21 +0500

    delete function from brain games js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 import { addName } from "../src/cli.js";
-export const brainGames = () => {
 
 addName();
-}
--- src/cli.js
+++ src/cli.js
@@ -7,3 +7,4 @@ export const addName = () => {
 
   return name;
 };
+    
